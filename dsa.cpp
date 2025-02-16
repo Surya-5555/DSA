d9c58e9fbdb037b45b40d0ebecbb3e99ab0437d6
surya <suryaravichandran5555@gmail.com>
2025-02-16 23:01:24 +0530

    New sum 1752 added

--- dsa.cpp
+++ dsa.cpp
@@ -60,3 +60,29 @@ class Solution {
         reverse(nums.begin(),nums.end());
         }
     };
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+1752. Check if Array Is Sorted and Rotated
+class Solution {
+    public:
+        bool check(vector<int>& nums) {
+            int countBreaks = 0; 
+            int n = nums.size();
+    
+            for (int i = 1; i < n; i++) {
+                if (nums[i] < nums[i - 1]) {
+                    countBreaks++;  
+                }
+            }
+    
+          
+            if (nums[n - 1] > nums[0]) {
+                countBreaks++;
+            }
+    
+           
+            return countBreaks <= 1;
+        }
+    };
